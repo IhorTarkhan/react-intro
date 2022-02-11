e1db1f1c0995033002112de13b96a0f619e0df1e
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-02-11 18:38:12 +0200

    sample component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Header } from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={"/logo512.png"} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Header />
+      <Header />
+        sdxvsdfsdz
+      <Header />
     </div>
   );
 }
